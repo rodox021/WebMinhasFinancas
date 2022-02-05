ac47301554145bf23a055e818a73f4193a7f8f4d
Rodolfo Leal Braga <75742317+rodox021@users.noreply.github.com>
2022-02-05 17:17:21 -0300

    Criado a migration inicial com user e alterado tema 5.2.22
    Foi criado a migrations e user e atualizado no banco de dados.
    alterado o tema do projeto.

--- WebMinhaFinancas/WebMinhaFinancas/Startup.cs
+++ WebMinhaFinancas/WebMinhaFinancas/Startup.cs
@@ -37,7 +37,8 @@ namespace WebMinhaFinancas
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<WebMinhaFinancasContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("WebMinhaFinancasContext")));
+                    options.UseMySql(Configuration.GetConnectionString("WebMinhaFinancasContext"),
+                    builder => builder.MigrationsAssembly("WebMinhaFinancas")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
